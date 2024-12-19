1b010ee93f4e1404dabeea19efe918aa73c57bb8
Aleksandra <zubarevaoleksandra05@gmail.com>
2024-12-19 08:27:19 +0200

    added page "not found"

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import ComicsPage from '../../pages/ComicsPage';
 import MainPage from '../../pages/MainPage';
+import NotFound from '../../pages/NotFound'
 import AppHeader from '../appHeader/AppHeader';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -13,6 +14,7 @@ const App = () => {
           <Routes>
             <Route path='/' element={<MainPage/>} />
             <Route path='/comics' element={<ComicsPage/>} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
       </div>
